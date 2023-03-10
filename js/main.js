6b9d55fd5cf98aaffa1bb26cf6cb0fdf2549e254
devthiart <devthiart@gmail.com>
2023-03-10 12:03:04 -0300

    Class 03

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
-const list = document.getElementById('lista');
-const form = document.getElementById('novoItem');
-const items = [];
+const list = document.getElementById('list');
+const form = document.getElementById('newItem');
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+items.forEach(element => {
+  createElement(element);
+})
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const name = event.target.elements['nome'];
-  const quantity = event.target.elements['quantidade'];
-
-  createElement(name.value, quantity.value);
-
-  name.value = '';
-  quantity.value = '';
-});
-
-function createElement(name, quantity) {
-  const newItem = document.createElement('li');
-  newItem.classList.add('item');
-
-  const itemQuantity = document.createElement('strong');
-  itemQuantity.innerHTML = quantity;
-
-  newItem.appendChild(itemQuantity);
-  newItem.innerHTML += name;
-  
-  list.appendChild(newItem);
+  const name = event.target.elements['name'];
+  const quantity = event.target.elements['quantity'];
 
   const currentItem = {
-    'nome': name,
-    'quantidade': quantity,
+    'name': name.value,
+    'quantity': quantity.value,
   }
 
   items.push(currentItem);
 
   localStorage.setItem('items', JSON.stringify(items));
+
+  createElement(currentItem);
+
+  name.value = '';
+  quantity.value = '';
+});
+
+function createElement(item) {
+  const newItem = document.createElement('li');
+  newItem.classList.add('item');
+
+  const itemQuantity = document.createElement('strong');
+  itemQuantity.innerHTML = item.quantity;
+
+  newItem.appendChild(itemQuantity);
+  newItem.innerHTML += item.name;
+  
+  list.appendChild(newItem);
 }
